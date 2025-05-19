d7ee019e83abb4c7e105276adc665f6441bd783e
Nylon1 <71017473+Nylon1@users.noreply.github.com>
2025-05-19 01:26:04 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors *"
+          }
+        ]
+      }
+    ];
+  }
+};
+
+module.exports = nextConfig;
+
